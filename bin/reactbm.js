585c837aa236018e0ee72f991b02a65335d49c95
erikEBM <howl_bm96@hotmail.com>
2016-11-14 23:07:05 -0500

    sdgf

--- bin/reactbm.js
+++ bin/reactbm.js
@@ -1,9 +1,35 @@
 #!/usr/bin/env node
 
+
+
 const readline = require('readline');
 const fs = require('fs');
 const exec = require('child_process').exec;
 
+function capitalize(cadena){
+  return cadena[0].toLocaleUpperCase + cadena.slice(1);
+}
+
+const path = require("path");
+const proceso = capitalize(process.argv[2]);
+const rutaproject = path.resolve(__dirname) + "/..";
+const rutaabsoluta = path.resolve(__dirname);
+
+console.log(proceso);
+
+fs.readFile( rutaproject  + "/snip/component",  "utf8", (err, fd)=>{
+         let indata = fd;
+         let outdata = indata.replace(/component/g, proceso[2]);
+         console.log(outdata);
+        fs.writeFile( "./src/components/" + proceso[2] + ".js", outdata, (err)=>{
+            if (err) throw err;
+            console.log('está guardado');
+        })
+})
+
+
+
+/*
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -30,3 +56,4 @@ rl.question('what name do you want choose?', (answer) => {
 
 });
 
+*/
